eb1ba9bb8236ae8efbe8b68bd26737b1d6857208
voli <vadim.oliinyk@gmail.com>
2024-08-09 22:19:58 +0000

    Initial commit

---
+++ components/liba/src/com/demo/liba/interface.clj
@@ -0,0 +1,5 @@
+(ns com.demo.liba.interface
+  (:gen-class))
+
+(defn -main [& _args]
+  (println "Hello from Demo Project!!!"))
---
+++ components/liba/test/com/demo/liba/interface_test.clj
@@ -0,0 +1,6 @@
+(ns com.demo.liba.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [com.demo.liba.interface :as liba]))
+
+(deftest dummy-test
+  (is (= 1 1)))
